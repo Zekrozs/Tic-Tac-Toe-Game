7eeff53ba19d0270d2f5406b39909d07a67528b4
Zaid Shuqair <zaidshuqairbizz@gmail.com>
2026-09-07 17:27:48 +0300

    added gameController Func updated paintscreen func

--- TTT.js
+++ TTT.js
@@ -1,23 +1,66 @@
 "use strict";
-const dialog = document.getElementById('dialog-box')
-dialog.showModal()  
 
-function DOM(){
-  const form = document.getElementById('form')
-  const formData = new FormData(form) 
-  const formValues = Object.fromEntries(formData)
-  const playersScreenNames = document.querySelectorAll('.name')
-  
-  const allPlayers  = Object.values(formValues)
+let DOM = (() => {
+  const form = document.getElementById("form");
+  const formData = new FormData(form);
+  const formValues = Object.fromEntries(formData);
+  const playersScreenNames = document.querySelectorAll(".name");
+  const dialog = document.getElementById("dialog-box");
+  const mainPage = document.querySelector(".main-container");
 
+  return { formValues, playersScreenNames, dialog, form, dialog,mainPage };
+})();
 
-const updateNames = () => {
-  playersScreenNames.forEach((name,index) => name.textContent = allPlayers[index])
-}
+const gameController = (() => {
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const allNames = getPlayersNames()
+    const validated = validation(allNames)
+    if(!validated) return
+    paintScreen.updateNames(allNames);
+    const game = gameState(allNames);
+    closeDialog()
 
- return{updateNames,allPlayers}
-}
+  };
 
+  const getPlayersNames = () => {
+    const formData = new FormData(DOM.form);
+    const formValues = Object.fromEntries(formData);
+    return Object.values(formValues);
+  };
+
+  const validation = (names) =>{
+    const emptyFields = names.some(field => field === '' )
+    if(emptyFields){
+      alert('all fields are required')
+      return false
+    }
+    return true
+  }
+
+  const openDialog = () => DOM.dialog.showModal();
+  const closeDialog = () => DOM.dialog.close()
+  DOM.form.addEventListener("submit", handleFormSubmit);
+
+  DOM.mainPage.addEventListener("click", (e) => {
+    const target = e.target;
+    const dialogBtn = target.closest('[data-game="show-modal"]');
+
+    if (dialogBtn) {
+      openDialog();
+    }
+  });
+})();
+
+const paintScreen = (() => {
+  const allPlayers = Object.values(DOM.formValues);
+  const updateNames = (names) => {
+    DOM.playersScreenNames.forEach(
+      (name, index) => (name.textContent = names[index]),
+    );
+  };
+  return { updateNames, allPlayers };
+})();
 
 const board = (function gameBoard() {
   const rows = 3;
@@ -33,21 +76,18 @@ const board = (function gameBoard() {
   const getBoard = () => board;
 
   const markSquare = function (row, column, player) {
-    if(row < 0 || row > 2 || column < 0 || column > 2){
-      console.log('please mark a valid cell')
-      return false
-    }  
+    if (row < 0 || row > 2 || column < 0 || column > 2) {
+      console.log("please mark a valid cell");
+      return false;
+    }
     if (board[row][column].getValue() !== 0) {
       console.log("cell is already marked");
       return false;
     }
 
-    
     board[row][column].markCell(player);
     return true;
-    } 
-      
-  
+  };
 
   // creates a 2d array. .map creates an array and throws the result
   // of the loop inside it.
@@ -71,23 +111,25 @@ function player(name, mark) {
   const player = {
     name: name,
     mark: mark,
-    roundsWon: 0
+    roundsWon: 0,
   };
   const getPlayerMark = () => player.mark;
   const getPlayerName = () => player.name;
-  const incrementWonRounds = () => player.roundsWon++
-  const getPlayerScore = () =>player.roundsWon
-  const resetScore = () => player.roundsWon = 0
-  return { getPlayerMark, getPlayerName,incrementWonRounds,getPlayerScore, resetScore };
+  const incrementWonRounds = () => player.roundsWon++;
+  const getPlayerScore = () => player.roundsWon;
+  const resetScore = () => (player.roundsWon = 0);
+  return {
+    getPlayerMark,
+    getPlayerName,
+    incrementWonRounds,
+    getPlayerScore,
+    resetScore,
+  };
 }
 
-function gameState() {
-  const playersNames = DOM()
-  const playerOne = player(playersNames.allPlayers[0], "X");
-  const playerTwo = player(playersNames.allPlayers[1], "O");
-  playersNames.updateNames()
-
-
+function gameState(players) {
+  const playerOne = player(players[0], "X");
+  const playerTwo = player(players[1], "O");
   let activePLayer = playerOne;
   const switchTurn = () => {
     if (activePLayer === playerOne) {
@@ -100,7 +142,7 @@ function gameState() {
   const checkWinner = () => {
     const currentBoard = board.printBoard();
     const playerMark = activePLayer.getPlayerMark();
-    const increaseScore = () => activePLayer.incrementWonRounds()
+    const increaseScore = () => activePLayer.incrementWonRounds();
     const winningLines = [
       ...currentBoard,
 
@@ -116,11 +158,10 @@ function gameState() {
       return line.every((cell) => cell === playerMark);
     });
 
-
     if (winner) {
       console.log(`${activePLayer.getPlayerName()} wins!`);
-      increaseScore()
-      console.log(activePLayer.getPlayerScore())
+      increaseScore();
+      console.log(activePLayer.getPlayerScore());
       return true;
     }
 
@@ -141,13 +182,13 @@ function gameState() {
     board.getBoard().forEach((row) => {
       row.forEach((cell) => cell.markCell());
     });
-    playerOne.resetScore()
-    playerTwo.resetScore()
+    playerOne.resetScore();
+    playerTwo.resetScore();
     console.log(board.printBoard());
   };
 
   const playRound = (row, column) => {
-        console.log(`${activePLayer.getPlayerName()}'s turn`);
+    console.log(`${activePLayer.getPlayerName()}'s turn`);
     if (!board.markSquare(row, column, activePLayer.getPlayerMark())) return;
     console.log(
       `${activePLayer.getPlayerName()} marked row ${row + 1} column ${column + 1}`,
@@ -160,7 +201,7 @@ function gameState() {
   return { playRound, resetGame };
 }
 
-const game = gameState();
+    
 game.playRound(0, 0); // X
 game.playRound(1, 1); // O
 game.playRound(0, 1); // X
@@ -172,6 +213,3 @@ game.playRound(1, 1); // O
 game.playRound(0, 1); // X
 game.playRound(1, 2); // O
 game.playRound(0, 2); // X
-
-
-
